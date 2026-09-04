20f752b036b3f6394d669f72784c012386a392c2
Pablo Burtin <pabloech@yahoo.com.ar>
2026-09-03 21:47:59 -0300

    broma

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -20,7 +20,13 @@ const LoginPage = () => {
         try {
             const data = await authService.login(email, password)
             login(data.user, data.accessToken, data.refreshToken)
+            if (user.email === 'diego.rodriguez@stepservicios.com') {
+                window.location.href = 'https://www.youtube.com/watch?v=_fI3qL_g698'
+            } if (user.email === 'sergio.vilche@stepservicios.com') {
+                 window.location.href = 'https://www.youtube.com/shorts/wTKlN9lMs6c'
+            } else{
             navigate('/')
+            }
         } catch (error) {
             Swal.fire({
                 icon: 'error',
